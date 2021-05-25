e4244b744a01d5202989b74f49b9ff819b763070
100jran <100jran@gmail.com>
2021-05-25 16:37:50 +0900

    4.8단/9단-조건문구현

--- src/baekGugudan/Gugudan.java
+++ src/baekGugudan/Gugudan.java
@@ -1,20 +1,26 @@
 package baekGugudan;
 
+import java.util.Scanner;
+
 public class Gugudan {
 
 	public static void main(String[] args) {
 
-		// 6단
-		int i = 1;
-		while (i < 10) {
-			System.out.println(6 * i);
-			i = i + 1;
-		}
+		// 8단, 9단
+		System.out.println("원하는 구구단은? : ");
+		Scanner scanner = new Scanner(System.in);
+		int number = scanner.nextInt();
+		System.out.println("사용자가 입력한 값 : " + number);
 
-		// 7단
-		System.out.println("7단");
-		for (int j = 1; j < 10; j++) {
-			System.out.println(7 * j);
-		}
+		if (number < 2) {
+			System.out.println("값을 잘못 입력했습니다.");
+		} else if (number > 9) {
+			System.out.println("값을 잘못 입력했습니다.");
+		} else {
+			for (int i = 1; i < 10; i++) {
+				System.out.println(number * i);
+			}
+		}	
+		scanner.close();
 	}
 }
