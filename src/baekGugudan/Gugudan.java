547d785e6dd3b446c89ff3be27425c5eaf5e8142
100jran <100jran@gmail.com>
2021-05-25 16:42:45 +0900

    5.2단-배열구현

--- src/baekGugudan/Gugudan.java
+++ src/baekGugudan/Gugudan.java
@@ -1,26 +1,13 @@
 package baekGugudan;
 
-import java.util.Scanner;
-
 public class Gugudan {
 
 	public static void main(String[] args) {
 
-		// 8단, 9단
-		System.out.println("원하는 구구단은? : ");
-		Scanner scanner = new Scanner(System.in);
-		int number = scanner.nextInt();
-		System.out.println("사용자가 입력한 값 : " + number);
-
-		if (number < 2) {
-			System.out.println("값을 잘못 입력했습니다.");
-		} else if (number > 9) {
-			System.out.println("값을 잘못 입력했습니다.");
-		} else {
-			for (int i = 1; i < 10; i++) {
-				System.out.println(number * i);
-			}
-		}	
-		scanner.close();
+		int[] result = new int[9];
+		for (int i = 0; i < result.length; i++) {
+			result[i] = 2 * (i + 1);
+			System.out.println(result[i]);
+		}
 	}
 }
