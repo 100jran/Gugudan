a25fc8b296f8396ce92bec5de372216da532f150
100jran <100jran@gmail.com>
2021-05-25 17:33:16 +0900

    6.9단-메소드구현

--- src/baekGugudan/Gugudan.java
+++ src/baekGugudan/Gugudan.java
@@ -2,11 +2,21 @@ package baekGugudan;
 
 public class Gugudan {
 
-	public static void main(String[] args) {
-
+	public static int[] calculate(int times) {
+		
 		int[] result = new int[9];
+
 		for (int i = 0; i < result.length; i++) {
-			result[i] = 2 * (i + 1);
+			result[i] = times * (i + 1);
+		}
+		return result;
+	}
+
+	public static void main(String[] args) {
+		
+		int[] result = calculate(9);
+
+		for (int i = 0; i < result.length; i++) {
 			System.out.println(result[i]);
 		}
 	}
