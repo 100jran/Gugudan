b891351435bddd12ba0b967318fbf95e699cb19d
100jran <100jran@gmail.com>
2021-05-25 17:34:10 +0900

    7.5단-메소드구현

--- src/baekGugudan/Gugudan.java
+++ src/baekGugudan/Gugudan.java
@@ -3,7 +3,7 @@ package baekGugudan;
 public class Gugudan {
 
 	public static int[] calculate(int times) {
-		
+
 		int[] result = new int[9];
 
 		for (int i = 0; i < result.length; i++) {
@@ -13,11 +13,11 @@ public class Gugudan {
 	}
 
 	public static void main(String[] args) {
-		
-		int[] result = calculate(9);
 
-		for (int i = 0; i < result.length; i++) {
-			System.out.println(result[i]);
+		int[] times5 = calculate(5);
+
+		for (int i = 0; i < times5.length; i++) {
+			System.out.println(times5[i]);
 		}
 	}
 }
