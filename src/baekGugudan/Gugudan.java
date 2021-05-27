08260e9b2696b5812103ebebab3d03e2b4fa7f21
100jran <100jran@gmail.com>
2021-05-27 20:21:31 +0900

    12.nm단-n단까지m만큼구현

--- src/baekGugudan/Gugudan.java
+++ src/baekGugudan/Gugudan.java
@@ -3,29 +3,22 @@ package baekGugudan;
 import java.util.Scanner;
 
 public class Gugudan {
-
-	public static int[] calculate(int times) {
-		int[] result = new int[9];
-
-		for (int i = 0; i < result.length; i++) {
-			result[i] = times * (i + 1);
-		}
-		return result;
-	}
-
-	public static void print(int[] result) {
-		for (int i = 0; i < result.length; i++) {
-			System.out.println(result[i]);
-		}
-	}
-
 	public static void main(String[] args) {
-		System.out.println("몇 단?");
+		
+		System.out.println("입력하세요. " + "a,b");
+		
 		Scanner scanner = new Scanner(System.in);
-		int number = scanner.nextInt();
-
-		for (int i = 2; i <= number; i++) {
-			for (int j = 1; j <= number; j++) {
+		
+		String inputValue = scanner.nextLine();
+		
+		String[] splitedValue = inputValue.split(",");
+		
+		int first = Integer.parseInt(splitedValue[0]);
+		int second = Integer.parseInt(splitedValue[1]);
+		System.out.println(first + "단까지 " + second + "만큼 곱하겠습니다!");
+		
+		for(int i = 2; i <= first; i++) {
+			for (int j = 1; j <= second; j++) {
 				System.out.println(i + "X" + j + "=" + i * j);
 			}
 		}
